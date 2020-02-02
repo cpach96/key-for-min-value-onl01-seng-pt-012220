039659403e23da66d8870310e793dfa9f419fb8c
Curtis pachtinger <cpach96@gmail.com>
2020-02-02 16:39:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 0 
+  min_key = nil
   name_hash.collect do |name , value|
     if value <=> value
       return name
